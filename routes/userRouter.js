47708a57ad638cd8bbe97083304fd75c9778b723
Ahmed ALHalwagy <ahmedhalwgy@gmail.com>
2023-09-05 18:18:42 +0300

    Handle added signup and login and permision

---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+const express = require('express'); // Import the Express framework
+const authController = require('../controllers/authController'); // Import the authentication controller
+
+const router = express.Router(); // Create an Express router
+
+// Define a route for user signup at the path '/signup'
+// When a POST request is made to this route, it will be handled by the 'signupUser' function from the 'authController'
+router.route('/signup').post(authController.signupUser);
+
+// Define a route for user login at the path '/login'
+// When a POST request is made to this route, it will be handled by the 'login' function from the 'authController'
+router.route('/login').post(authController.login);
+
+// Export the router for use in other parts of your application
+module.exports = router;
